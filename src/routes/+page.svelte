3b2f94ea2219ee5c2d7c53c3f8ea14f668afbbe9
Daniele Rolli <danielerolli004@gmail.com>
2023-02-04 23:03:37 +0100

    dark mode

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,6 +5,7 @@
 <!-- stylesheets -->
 <link rel="stylesheet" href="../src/css/navbar.css">
 <link rel="stylesheet" href="../src/css/index.css">
+<link rel="stylesheet" href="../src/css/mode.css">
 
 <!-- Load an icon library -->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
@@ -17,8 +18,8 @@
   <a href="#"><i class="fas fa-arrow-left"></i></a> 
   <a href="1"><i class="fa-solid fa-arrow-right"></i></a>
   <a href="#"><i class="fa fa-fw fa-search"></i></a> 
-  <button>
-    <i class="fa-solid fa-lightbulb"></i>
+  <button id="toggle-dark-mode">
+    <i class="fa-solid fa-moon"></i>
     </button>
 </div>
 
@@ -34,5 +35,25 @@
 </div>
 
   </body>
-
+  <script>
+    const toggleButton = document.getElementById('toggle-dark-mode');
+    toggleButton.addEventListener('click', () => {
+      document.body.classList.toggle('dark-mode');
+    });
+  </script>
+   <script>
+    const body = document.querySelector('body');
+    const prefersDark = window.matchMedia('(prefers-color-scheme: dark)');
+    
+    function handleDarkPreferences(event) {
+      if (event.matches) {
+        body.classList.add('dark-mode');
+      } else {
+        body.classList.remove('dark-mode');
+      }
+    }
+    
+    prefersDark.addListener(handleDarkPreferences);
+    handleDarkPreferences(prefersDark);
+  </script>
 </html>
